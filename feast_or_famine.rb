2e16beeff599cfa7c0acbf8eeb5cc2a9775f016c
Kyle Thousand <kylethousand@kyles-mbp-2.lan>
2020-11-22 11:06:56 -0500

    Kyle Thousand

--- feast_or_famine.rb
+++ feast_or_famine.rb
@@ -3,9 +3,15 @@
 # Pseudocode here.
 
 def feast_or_famine(beast, dish)
-
+  beast_arr = [beast.split('').first, beast.split('').last]
+  dish_arr = [dish.split('').first, dish.split('').last]
+  if beast_arr.join('') == dish_arr.join('')
+    beast_arr.join('')
+  else
+    false
+  end
 end
 
 # Print here.
 
-puts feast("great blue heron", "garlic naan")
+puts feast_or_famine("great blue heron", "garlic naan")
